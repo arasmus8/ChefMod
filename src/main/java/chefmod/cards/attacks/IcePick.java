ff9a0a7dc3c0816bd391e4fc2001824a95bd7c51
arasmus8 <arasmus8@gmail.com>
2020-09-02 16:37:04 -0600

    feat: new card

---
+++ src/main/java/chefmod/cards/attacks/IcePick.java
@@ -0,0 +1,31 @@
+package chefmod.cards.attacks;
+
+import chefmod.actions.FreezeAction;
+import chefmod.cards.AbstractChefCard;
+import chefmod.powers.HungerPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static chefmod.ChefMod.makeID;
+
+public class IcePick extends AbstractChefCard {
+    public static String ID = makeID(IcePick.class.getSimpleName());
+
+    public IcePick() {
+        super(ID,
+                1,
+                CardType.ATTACK,
+                CardRarity.COMMON,
+                CardTarget.ENEMY
+        );
+        baseDamage = damage = 7;
+        upgradeDamageBy = 3;
+        damages = true;
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dealDamage(m, AbstractGameAction.AttackEffect.FIRE);
+        addToBot(new FreezeAction(1));
+    }
+}
