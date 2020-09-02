26fb023f2458650e298248a1fd3b70a9ac2fa3fd
arasmus8 <arasmus8@gmail.com>
2020-09-02 16:04:22 -0600

    feat: new attacks

---
+++ src/main/java/chefmod/cards/attacks/FullBurn.java
@@ -0,0 +1,36 @@
+package chefmod.cards.attacks;
+
+import chefmod.cards.AbstractChefCard;
+import chefmod.powers.HungerPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static chefmod.ChefMod.makeID;
+
+public class FullBurn extends AbstractChefCard {
+    public static String ID = makeID(FullBurn.class.getSimpleName());
+
+    public FullBurn() {
+        super(ID,
+                3,
+                CardType.ATTACK,
+                CardRarity.COMMON,
+                CardTarget.ALL_ENEMY
+        );
+        baseDamage = damage = 23;
+        upgradeDamageBy = 5;
+        baseMagicNumber = magicNumber = 1;
+        upgradeMagicNumberBy = 1;
+        damages = true;
+        isMultiDamage = true;
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dealAoeDamage(AbstractGameAction.AttackEffect.FIRE);
+        if (isPrepped()) {
+            addToBot(new GainEnergyAction(magicNumber));
+        }
+    }
+}
