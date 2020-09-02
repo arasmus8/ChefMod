f571356b8ca8c3a1e5f1127798e6e816138cd1ad
arasmus8 <arasmus8@gmail.com>
2020-09-02 15:53:55 -0600

    feat: new attacks

---
+++ src/main/java/chefmod/cards/attacks/Fillet.java
@@ -0,0 +1,30 @@
+package chefmod.cards.attacks;
+
+import chefmod.actions.FreezeAction;
+import chefmod.cards.AbstractChefCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static chefmod.ChefMod.makeID;
+
+public class Fillet extends AbstractChefCard {
+    public static String ID = makeID(Fillet.class.getSimpleName());
+
+    public Fillet() {
+        super(ID,
+                0,
+                CardType.ATTACK,
+                CardRarity.COMMON,
+                CardTarget.ENEMY
+        );
+        baseDamage = damage = 5;
+        upgradeDamageBy = 3;
+        damages = true;
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dealDamage(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
+        addToBot(new FreezeAction(this.makeStatEquivalentCopy()));
+    }
+}
---
+++ src/main/java/chefmod/cards/attacks/FireUp.java
@@ -0,0 +1,31 @@
+package chefmod.cards.attacks;
+
+import chefmod.actions.FreezeAction;
+import chefmod.cards.AbstractChefCard;
+import chefmod.powers.HungerPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static chefmod.ChefMod.makeID;
+
+public class FireUp extends AbstractChefCard {
+    public static String ID = makeID(FireUp.class.getSimpleName());
+
+    public FireUp() {
+        super(ID,
+                1,
+                CardType.ATTACK,
+                CardRarity.COMMON,
+                CardTarget.ENEMY
+        );
+        baseDamage = damage = 9;
+        upgradeDamageBy = 3;
+        damages = true;
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dealDamage(m, AbstractGameAction.AttackEffect.FIRE);
+        applyToEnemy(m, new HungerPower(m));
+    }
+}
