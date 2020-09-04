1c227707fc523c709d6c86e6febb2c3a603288f8
arasmus8 <arasmus8@gmail.com>
2020-09-04 17:12:36 -0600

    feat: new card

---
+++ src/main/java/chefmod/cards/attacks/DoubleChop.java
@@ -0,0 +1,45 @@
+package chefmod.cards.attacks;
+
+import chefmod.ChefMod;
+import chefmod.actions.FreezeAction;
+import chefmod.cards.AbstractChefCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static chefmod.ChefMod.makeID;
+
+public class DoubleChop extends AbstractChefCard {
+    public static String ID = makeID(DoubleChop.class.getSimpleName());
+
+    public DoubleChop() {
+        super(ID,
+                1,
+                CardType.ATTACK,
+                CardRarity.UNCOMMON,
+                CardTarget.ENEMY
+        );
+        baseDamage = damage = 4;
+        baseMagicNumber = magicNumber = 2;
+        upgradeDamageBy = 1;
+        damages = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0; i < magicNumber; i++) {
+            dealDamage(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
+        }
+    }
+
+    @Override
+    public void triggerOnEndOfPlayerTurn() {
+        super.triggerOnEndOfPlayerTurn();
+        ChefMod.cardsToFreeze.add(this);
+    }
+
+    @Override
+    public void onRetained() {
+        addToBot(new FreezeAction(this));
+    }
+}
