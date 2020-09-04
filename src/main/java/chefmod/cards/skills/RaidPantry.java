cb8e6d00b811edfb314b069e03ee059104276f11
arasmus8 <arasmus8@gmail.com>
2020-09-04 15:51:01 -0600

    feat: new card

---
+++ src/main/java/chefmod/cards/skills/RaidPantry.java
@@ -0,0 +1,39 @@
+package chefmod.cards.skills;
+
+import chefmod.ChefMod;
+import chefmod.actions.PlayOldestFrozenCardAction;
+import chefmod.cards.AbstractChefCard;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static chefmod.ChefMod.makeID;
+
+public class RaidPantry extends AbstractChefCard {
+    public static String ID = makeID(RaidPantry.class.getSimpleName());
+
+    public RaidPantry() {
+        super(ID,
+                2,
+                CardType.SKILL,
+                CardRarity.RARE,
+                CardTarget.SELF
+        );
+        upgradeCostTo = 1;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        List<AbstractCard> attacks = ChefMod.frozenPile.group.stream()
+                .filter(c -> c.type == CardType.ATTACK)
+                .collect(Collectors.toList());
+        ChefMod.frozenPile.group.removeIf(attacks::contains);
+        attacks.forEach(c -> ChefMod.frozenPile.addToBottom(c));
+        for (int i = 0; i < attacks.size(); i++) {
+            addToBot(new PlayOldestFrozenCardAction(false));
+        }
+    }
+}
