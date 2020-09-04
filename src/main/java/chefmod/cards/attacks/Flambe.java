605f860b8fcb480ad287d3e299aafd80441790fa
arasmus8 <arasmus8@gmail.com>
2020-09-04 17:19:10 -0600

    feat: new card

---
+++ src/main/java/chefmod/cards/attacks/Flambe.java
@@ -0,0 +1,34 @@
+package chefmod.cards.attacks;
+
+import chefmod.cards.AbstractChefCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static chefmod.ChefMod.makeID;
+
+public class Flambe extends AbstractChefCard {
+    public static String ID = makeID(Flambe.class.getSimpleName());
+
+    public Flambe() {
+        super(ID,
+                1,
+                CardType.ATTACK,
+                CardRarity.UNCOMMON,
+                CardTarget.ENEMY
+        );
+        baseDamage = damage = 12;
+        upgradeDamageBy = 4;
+        damages = true;
+        exhaust = true;
+        hasPreppedActions = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dealDamage(m, AbstractGameAction.AttackEffect.FIRE);
+        if (isPrepped()) {
+            shuffleIn(this.makeStatEquivalentCopy());
+        }
+    }
+}
