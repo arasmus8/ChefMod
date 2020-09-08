33bf4632aa7d324c448f5d3f792a7697dc672405
arasmus8 <arasmus8@gmail.com>
2020-09-08 13:14:04 -0600

    feat: new card

---
+++ src/main/java/chefmod/cards/attacks/KnifeFlurry.java
@@ -0,0 +1,33 @@
+package chefmod.cards.attacks;
+
+import chefmod.cards.AbstractChefCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.stream.IntStream;
+
+import static chefmod.ChefMod.makeID;
+
+public class KnifeFlurry extends AbstractChefCard {
+    public static String ID = makeID(KnifeFlurry.class.getSimpleName());
+
+    public KnifeFlurry() {
+        super(ID,
+                2,
+                CardType.ATTACK,
+                CardRarity.UNCOMMON,
+                CardTarget.ENEMY
+        );
+        baseDamage = damage = 4;
+        upgradeDamageBy = 2;
+        baseMagicNumber = magicNumber = 4;
+        damages = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        IntStream.rangeClosed(1, magicNumber)
+                .forEachOrdered(i -> dealDamage(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+    }
+}
