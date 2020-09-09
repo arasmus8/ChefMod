080a3df0808329e5f86ae6201e9f950fd12027d1
arasmus8 <arasmus8@gmail.com>
2020-09-09 08:58:19 -0600

    feat: new card

---
+++ src/main/java/chefmod/cards/attacks/QuickChop.java
@@ -0,0 +1,36 @@
+package chefmod.cards.attacks;
+
+import chefmod.actions.PlayOldestFrozenCardAction;
+import chefmod.cards.AbstractChefCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.stream.IntStream;
+
+import static chefmod.ChefMod.makeID;
+
+public class QuickChop extends AbstractChefCard {
+    public static String ID = makeID(QuickChop.class.getSimpleName());
+
+    public QuickChop() {
+        super(ID,
+                1,
+                CardType.ATTACK,
+                CardRarity.UNCOMMON,
+                CardTarget.ENEMY
+        );
+        baseDamage = damage = 7;
+        upgradeDamageBy = 2;
+        baseMagicNumber = magicNumber = 1;
+        upgradeMagicNumberBy = 2;
+        damages = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dealDamage(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
+        IntStream.rangeClosed(1, magicNumber)
+                .forEachOrdered(i -> addToBot(new PlayOldestFrozenCardAction(false)));
+    }
+}
