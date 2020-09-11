297941594f04ebcf6ced8a41362fba861a321fa8
arasmus8 <arasmus8@gmail.com>
2020-09-11 10:51:37 -0600

    feat: new card

---
+++ src/main/java/chefmod/recipe/LocoMocoRecipe.java
@@ -0,0 +1,46 @@
+package chefmod.recipe;
+
+import chefmod.actions.FunctionalAction;
+import chefmod.powers.SatiatedPower;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
+
+import static chefmod.ChefMod.makeID;
+
+public class LocoMocoRecipe extends AbstractRecipe {
+    public static final String ID = makeID(LocoMocoRecipe.class.getSimpleName());
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(ID);
+    public static final String[] TEXT = uiStrings.TEXT;
+
+    private final int drawAmount;
+    private final boolean reduceAttackCost;
+
+    public LocoMocoRecipe(int drawAmount, boolean reduceAttackCost) {
+        this.drawAmount = drawAmount;
+        this.reduceAttackCost = reduceAttackCost;
+        tipHeader = TEXT[0];
+        tipBody = TEXT[1] + (reduceAttackCost ? TEXT[3] : TEXT[2]);
+        ingredientCount = 2;
+    }
+
+    @Override
+    public void onActivate() {
+        monsterList().forEach(m -> applyToEnemy(m, new SatiatedPower(m)));
+        qAction(new DrawCardAction(drawAmount, new FunctionalAction(firstUpdate -> {
+            if (reduceAttackCost) {
+                DrawCardAction.drawnCards.stream()
+                        .filter(c -> c.type == AbstractCard.CardType.ATTACK)
+                        .forEach(c -> {
+                            c.cost = 0;
+                            c.costForTurn = 0;
+                            c.isCostModified = true;
+                            c.superFlash(Color.GOLD.cpy());
+                        });
+            }
+            return true;
+        })));
+    }
+}
