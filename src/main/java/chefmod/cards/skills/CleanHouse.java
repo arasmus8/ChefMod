80c65f0feae8fbf05a8d517264eb79e16e24d446
arasmus8 <arasmus8@gmail.com>
2020-09-11 14:56:41 -0600

    feat: new card

---
+++ src/main/java/chefmod/cards/skills/CleanHouse.java
@@ -0,0 +1,30 @@
+package chefmod.cards.skills;
+
+import chefmod.actions.FreezeAction;
+import chefmod.cards.AbstractChefCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static chefmod.ChefMod.makeID;
+
+public class CleanHouse extends AbstractChefCard {
+    public static String ID = makeID(CleanHouse.class.getSimpleName());
+
+    public CleanHouse() {
+        super(ID,
+                1,
+                CardType.SKILL,
+                CardRarity.UNCOMMON,
+                CardTarget.SELF
+        );
+        baseBlock = block = 8;
+        upgradeBlockBy = 3;
+        baseMagicNumber = magicNumber = 3;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        gainBlock();
+        addToBot(new FreezeAction(magicNumber, c -> c.type == CardType.CURSE || c.type == CardType.STATUS));
+    }
+}
