28df0f9f2aeacb4226b12ee4dba4509f205ac618
arasmus8 <arasmus8@gmail.com>
2020-09-11 14:38:14 -0600

    feat: new card

---
+++ src/main/java/chefmod/cards/skills/AlooGobi.java
@@ -0,0 +1,35 @@
+package chefmod.cards.skills;
+
+import chefmod.cards.AbstractChefCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static chefmod.ChefMod.makeID;
+
+public class AlooGobi extends AbstractChefCard {
+    public static String ID = makeID(AlooGobi.class.getSimpleName());
+
+    public AlooGobi() {
+        super(ID,
+                2,
+                CardType.SKILL,
+                CardRarity.UNCOMMON,
+                CardTarget.SELF
+        );
+        baseBlock = block = 10;
+        upgradeBlockBy = 3;
+        baseMagicNumber = magicNumber = 4;
+        upgradeMagicNumberBy = 2;
+    }
+
+    @Override
+    public void triggerWhenFrozen() {
+        baseBlock += magicNumber;
+        applyPowers();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        gainBlock();
+    }
+}
