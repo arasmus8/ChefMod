ab822c37d6bd5b130178c7e9864c5f7c3ac400e5
arasmus8 <arasmus8@gmail.com>
2020-09-11 14:50:06 -0600

    feat: new card

---
+++ src/main/java/chefmod/cards/skills/Bruschetta.java
@@ -0,0 +1,29 @@
+package chefmod.cards.skills;
+
+import chefmod.cards.AbstractChefCard;
+import chefmod.powers.HungerPower;
+import chefmod.powers.RetainThisTurnPower;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static chefmod.ChefMod.makeID;
+
+public class Bruschetta extends AbstractChefCard {
+    public static String ID = makeID(Bruschetta.class.getSimpleName());
+
+    public Bruschetta() {
+        super(ID,
+                1,
+                CardType.SKILL,
+                CardRarity.UNCOMMON,
+                CardTarget.ENEMY
+        );
+        upgradeCostTo = 0;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        applyToEnemy(m, new HungerPower(m));
+        applyToSelf(new RetainThisTurnPower(p, 1));
+    }
+}
