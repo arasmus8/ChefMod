33e23013b0fac66811dc61e1083b7d5d12b81103
arasmus8 <arasmus8@gmail.com>
2020-09-18 16:24:44 -0600

    feat: new card

---
+++ src/main/java/chefmod/cards/powers/TastyDistraction.java
@@ -0,0 +1,27 @@
+package chefmod.cards.powers;
+
+import chefmod.cards.AbstractChefCard;
+import chefmod.powers.TastyDistractionPower;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static chefmod.ChefMod.makeID;
+
+public class TastyDistraction extends AbstractChefCard {
+    public static String ID = makeID(TastyDistraction.class.getSimpleName());
+
+    public TastyDistraction() {
+        super(ID,
+                3,
+                CardType.POWER,
+                CardRarity.RARE,
+                CardTarget.SELF
+        );
+        upgradeCostTo = 2;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        applyToSelf(new TastyDistractionPower(p, 1));
+    }
+}
