7c0f18df6548758623ee20245fb2f2da01cd76e8
arasmus8 <arasmus8@gmail.com>
2020-10-19 13:09:08 -0600

    fix(ui): add vfx

--- src/main/java/chefmod/cards/attacks/ReptoDagger.java
+++ src/main/java/chefmod/cards/attacks/ReptoDagger.java
@@ -1,8 +1,14 @@
 package chefmod.cards.attacks;
 
 import chefmod.cards.AbstractChefCard;
+import chefmod.vfx.VfxBuilder;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import java.util.stream.IntStream;
@@ -11,6 +17,8 @@ import static chefmod.ChefMod.makeID;
 
 public class ReptoDagger extends AbstractChefCard {
     public static String ID = makeID(ReptoDagger.class.getSimpleName());
+    private static final TextureAtlas daggerAtlas = new TextureAtlas(Gdx.files.internal("images/monsters/theForest/mage_dagger/skeleton.atlas"));
+    private static final TextureAtlas.AtlasRegion dagger = daggerAtlas.findRegion("dagger copy");
 
     public ReptoDagger() {
         super(ID,
@@ -28,9 +36,24 @@ public class ReptoDagger extends AbstractChefCard {
         shuffleBackIntoDrawPile = true;
     }
 
+    private void daggerVfx(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new VFXAction(
+                new VfxBuilder(dagger, Settings.ACTION_DUR_FAST)
+                        .xRange(p.drawX, m.drawX, VfxBuilder.Interpolations.LINEAR)
+                        .yRange(MathUtils.random(p.hb.y, p.hb.y + p.hb.height), m.hb.cY, VfxBuilder.Interpolations.LINEAR)
+                        .setAngle(90f)
+                        .setScale(0.6f)
+                        .rotate(-1500f)
+                        .build()
+        ));
+    }
+
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        IntStream.rangeClosed(1, magicNumber).forEachOrdered(i -> dealDamage(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+        IntStream.rangeClosed(1, magicNumber).forEachOrdered(i -> {
+            daggerVfx(p, m);
+            dealDamage(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
+        });
         baseMagicNumber = magicNumber = magicNumber + 1;
     }
 }
