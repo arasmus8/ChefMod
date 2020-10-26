832d8834c4307fb7071de51d2a9f9b61a88effbf
arasmus8 <arasmus8@gmail.com>
2020-10-26 13:01:12 -0600

    feat(relic): add cookie jar

--- src/main/java/chefmod/relics/AbstractChefRelic.java
+++ src/main/java/chefmod/relics/AbstractChefRelic.java
@@ -1,13 +1,21 @@
 package chefmod.relics;
 
 import basemod.abstracts.CustomRelic;
+import chefmod.util.ActionUnit;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 import static chefmod.ChefMod.*;
 
-public abstract class AbstractChefRelic extends CustomRelic {
+public abstract class AbstractChefRelic extends CustomRelic implements ActionUnit {
     public AbstractCard.CardColor color;
 
     public AbstractChefRelic(String setId, AbstractRelic.RelicTier tier, AbstractRelic.LandingSound sfx) {
@@ -38,8 +46,36 @@ public abstract class AbstractChefRelic extends CustomRelic {
         outlineImg = ImageMaster.loadImage(makeRelicOutlinePath(imgName));
     }
 
+    protected int getDamage() {
+        return 0;
+    }
+
+    protected int getBlock() {
+        return 0;
+    }
+
     @Override
     public String getUpdatedDescription() {
         return DESCRIPTIONS[0];
+    }
+
+    @Override
+    public DamageInfo makeDamageInfo(DamageInfo.DamageType type) {
+        return new DamageInfo(AbstractDungeon.player, getDamage(), type);
+    }
+
+    @Override
+    public void dealDamage(AbstractMonster m, AbstractGameAction.AttackEffect fx) {
+        qAction(new DamageAction(m, makeDamageInfo(DamageInfo.DamageType.NORMAL), fx));
+    }
+
+    @Override
+    public void dealAoeDamage(AbstractGameAction.AttackEffect fx) {
+        qAction(new DamageAllEnemiesAction(AbstractDungeon.player, DamageInfo.createDamageMatrix(getDamage()), DamageInfo.DamageType.NORMAL, fx));
+    }
+
+    @Override
+    public void gainBlock() {
+        qAction(new GainBlockAction(AbstractDungeon.player, AbstractDungeon.player, getBlock()));
     }
 }
---
+++ src/main/java/chefmod/relics/CookieJar.java
@@ -0,0 +1,26 @@
+package chefmod.relics;
+
+import chefmod.TheChef;
+import chefmod.powers.HungerDamageUpPower;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+import static chefmod.ChefMod.makeID;
+
+public class CookieJar extends AbstractChefRelic {
+    public static String ID = makeID(CookieJar.class.getSimpleName());
+
+    public CookieJar() {
+        super(ID, RelicTier.SHOP, LandingSound.SOLID, TheChef.Enums.CHEF_COLOR);
+    }
+
+    @Override
+    public void onEnterRoom(AbstractRoom room) {
+        setCounter(counter + 1);
+    }
+
+    @Override
+    public void atBattleStartPreDraw() {
+        applyToSelf(new HungerDamageUpPower(AbstractDungeon.player, counter));
+    }
+}
