8fe3471bf54a1d6a287f8c42862f3afcc6ae4e70
arasmus8 <arasmus8@gmail.com>
2020-11-18 13:26:01 -0700

    fix: use built-in method for repeating a card

--- src/main/java/chefmod/cardmods/PlayTwiceCardmod.java
+++ src/main/java/chefmod/cardmods/PlayTwiceCardmod.java
@@ -2,12 +2,10 @@ package chefmod.cardmods;
 
 import basemod.abstracts.AbstractCardModifier;
 import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import java.util.Optional;
@@ -45,23 +43,7 @@ public class PlayTwiceCardmod extends AbstractChefCardmod {
         // checking dontTriggerOnUseCard prevents a co-modification crash on time eater when playing cards twice from the frozen pile
         if (!card.purgeOnUse && !card.dontTriggerOnUseCard) {
             AbstractCard tmp = card.makeSameInstanceOf();
-            AbstractDungeon.player.limbo.addToBottom(tmp);
-            tmp.current_x = card.current_x;
-            tmp.current_y = card.current_y;
-            tmp.target_x = (float) Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
-            tmp.target_y = (float) Settings.HEIGHT / 2.0F;
-            if (target != null) {
-                tmp.calculateCardDamage((AbstractMonster) target);
-            }
-
-            tmp.purgeOnUse = true;
-            AbstractDungeon.actionManager.addCardQueueItem(
-                    new CardQueueItem(tmp,
-                            (AbstractMonster) target,
-                            card.energyOnUse,
-                            true,
-                            true),
-                    true);
+            GameActionManager.queueExtraCard(tmp, (AbstractMonster) action.target);
         }
     }
 
