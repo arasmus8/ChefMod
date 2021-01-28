7cbc4924118c086767f94ab27e76b74fd329745b
arasmus8 <arasmus8@gmail.com>
2021-01-28 12:07:11 -0700

    fix: mince triggers draw effects on skill drawn.

--- src/main/java/chefmod/cards/attacks/Mince.java
+++ src/main/java/chefmod/cards/attacks/Mince.java
@@ -1,12 +1,17 @@
 package chefmod.cards.attacks;
 
+import chefmod.actions.FunctionalAction;
 import chefmod.cards.AbstractChefCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
-import com.megacrit.cardcrawl.actions.utility.DrawPileToHandAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 import static chefmod.ChefMod.makeID;
@@ -36,6 +41,19 @@ public class Mince extends AbstractChefCard {
 
     @Override
     public void triggerWhenDrawn() {
-        addToBot(new DrawPileToHandAction(1, CardType.SKILL));
+        //addToBot(new DrawPileToHandAction(1, CardType.SKILL));
+        addToBot(new FunctionalAction(firstUpdate -> {
+            AbstractPlayer p = AbstractDungeon.player;
+            Predicate<AbstractCard> isSkill = c -> c.type == CardType.SKILL;
+            Optional<AbstractCard> skillInDraw = p.drawPile.group.stream()
+                    .filter(isSkill)
+                    .findFirst();
+            skillInDraw.ifPresent(c -> {
+                p.drawPile.removeCard(c);
+                p.drawPile.addToTop(c);
+                addToBot(new DrawCardAction(p, 1));
+            });
+            return true;
+        }));
     }
 }
