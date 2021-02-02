d0783546bece9fab19db1f83fbcbe267499a0e1b
arasmus8 <arasmus8@gmail.com>
2021-02-02 13:04:46 -0700

    fix(balance): Double Chop now puts a card from the discard pile to the top of the draw pile instead of freezing itself.

--- src/main/java/chefmod/cards/attacks/DoubleChop.java
+++ src/main/java/chefmod/cards/attacks/DoubleChop.java
@@ -1,16 +1,10 @@
 package chefmod.cards.attacks;
 
-import chefmod.ChefMod;
-import chefmod.actions.FreezeAction;
 import chefmod.cards.AbstractChefCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DiscardSpecificCardAction;
-import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.actions.unique.DiscardPileToTopOfDeckAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.EquilibriumPower;
-import com.megacrit.cardcrawl.relics.RunicPyramid;
 
 import java.util.stream.IntStream;
 
@@ -36,22 +30,6 @@ public class DoubleChop extends AbstractChefCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         IntStream.rangeClosed(1, magicNumber)
                 .forEach(i -> dealDamage(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL));
-    }
-
-    @Override
-    public void triggerOnEndOfPlayerTurn() {
-        if (isEthereal) {
-            addToTop(new ExhaustSpecificCardAction(this, AbstractDungeon.player.hand));
-        } else {
-            ChefMod.cardsToFreeze.add(this);
-            if (AbstractDungeon.player.hasRelic(RunicPyramid.ID) || AbstractDungeon.player.hasPower(EquilibriumPower.POWER_ID) || retain || selfRetain) {
-                addToTop(new DiscardSpecificCardAction(this, AbstractDungeon.player.hand));
-            }
-        }
-    }
-
-    @Override
-    public void onRetained() {
-        addToBot(new FreezeAction(this));
+        addToBot(new DiscardPileToTopOfDeckAction(p));
     }
 }
