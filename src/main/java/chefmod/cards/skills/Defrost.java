6d9953b3a640acd36ec3e00d5916f074e2ebca96
arasmus8 <arasmus8@gmail.com>
2022-04-04 12:07:43 -0600

    fix: rework Defrost+ -> now plays a chosen frozen card when upgraded instead of picking 2 frozen cards.

--- src/main/java/chefmod/cards/skills/Defrost.java
+++ src/main/java/chefmod/cards/skills/Defrost.java
@@ -3,6 +3,7 @@ package chefmod.cards.skills;
 import chefmod.ChefMod;
 import chefmod.actions.FunctionalAction;
 import chefmod.actions.GridSelectAndPerformAction;
+import chefmod.actions.PlayOldestFrozenCardAction;
 import chefmod.cards.AbstractChefCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -20,23 +21,34 @@ public class Defrost extends AbstractChefCard {
                 CardTarget.SELF
         );
         magicNumber = baseMagicNumber = 1;
-        upgradeMagicNumberBy = 1;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new GridSelectAndPerformAction(magicNumber,
-                cardStrings.EXTENDED_DESCRIPTION[0],
-                new FunctionalAction(firstUpdate -> {
-                    GridSelectAndPerformAction.selectedCards.forEach(c -> {
-                        ChefMod.frozenPile.moveToHand(c, ChefMod.frozenPile);
-                        if (c instanceof AbstractChefCard) {
-                            AbstractChefCard chefCard = (AbstractChefCard) c;
-                            chefCard.frozen = false;
-                        }
-                    });
-                    return true;
-                })
-        ));
+        if (upgraded) {
+            addToBot(new GridSelectAndPerformAction(magicNumber,
+                    cardStrings.EXTENDED_DESCRIPTION[0],
+                    new FunctionalAction(firstUpdate -> {
+                        GridSelectAndPerformAction.selectedCards.forEach(c -> addToBot(new PlayOldestFrozenCardAction(filterCard -> filterCard == c)));
+                        return true;
+                    })
+
+            ));
+        } else {
+            addToBot(new GridSelectAndPerformAction(magicNumber,
+                    cardStrings.EXTENDED_DESCRIPTION[0],
+                    new FunctionalAction(firstUpdate -> {
+                        GridSelectAndPerformAction.selectedCards.forEach(c -> {
+                            ChefMod.frozenPile.moveToHand(c, ChefMod.frozenPile);
+                            if (c instanceof AbstractChefCard) {
+                                AbstractChefCard chefCard = (AbstractChefCard) c;
+                                chefCard.frozen = false;
+                            }
+                            c.triggerWhenDrawn();
+                        });
+                        return true;
+                    })
+            ));
+        }
     }
 }
